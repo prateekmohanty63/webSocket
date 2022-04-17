d6b2b6c480392f394cd1fde2c01f3f1b39eed622
prateekmohanty63 <72243320+prateekmohanty63@users.noreply.github.com>
2022-04-17 17:16:44 +0530

    Fetched data from web socket

--- stream.py
+++ stream.py
@@ -1,3 +1,4 @@
+from matplotlib.font_manager import json_dump
 import websocket,json
 from config import *
 
@@ -13,11 +14,21 @@ def on_open(ws):
     }
     ws.send(json.dumps(auth_data))
 
+    channel_data={
+        "action":"subscribe",
+        "params":TICKERS
+    }
+
+    ws.send(json.dumps(channel_data))
+
 def on_message(ws,message):
     print("recieved message")
     print(message)
 
+def on_close(ws):
+    print("Closing connection")
 
-ws=websocket.WebSocketApp(socket,on_open=on_open,on_message=on_message)
+
+ws=websocket.WebSocketApp(socket,on_open=on_open,on_message=on_message,on_close=on_close)
 
 ws.run_forever()
